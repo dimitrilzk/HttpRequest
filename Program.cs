d8990c43d86830c816f028430b59acf1102e714c
dimitrilzk <dimitrilazuka95@gmail.com>
2024-02-26 17:41:52 +0100

    end http

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Primitives;
 using System.IO;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -29,13 +31,29 @@ app.Run(async (HttpContext context) =>
     //    }
     //}
 
-    if (context.Request.Headers.ContainsKey("User-Agent"))
+    //if (context.Request.Headers.ContainsKey("User-Agent"))
+    //{
+    //    string userAgent = context.Request.Headers["User-Agent"];
+    //    await context.Response.WriteAsync($"<h1>{userAgent}</h1>");
+    //}
+
+    //if (context.Request.Headers.ContainsKey("AuthorizationKey"))
+    //{
+    //    string key = context.Request.Headers["AuthorizationKey"];
+    //    await context.Response.WriteAsync($"<h1>{key}</h1>");
+    //}
+
+    StreamReader reader = new StreamReader(context.Request.Body);
+    string body = await reader.ReadToEndAsync();
+    //se si sta mandando in post una query string per capire 
+    //chiave valore bisogna parsare la stringa che arriva nel body es: firstName=scott&age=20
+    Dictionary<string, StringValues> queryDic = QueryHelpers.ParseQuery(body);
+    if (queryDic.ContainsKey("firstName"))
     {
-        string userAgent = context.Request.Headers["User-Agent"];
-        await context.Response.WriteAsync($"<h1>{userAgent}</h1>");
+        string fistName = queryDic["firstName"][0];
+        await context.Response.WriteAsync($"<h1>{fistName}</h1>");
     }
 
-    await context.Response.WriteAsync($"<h1>Hello</h1>");
     await context.Response.WriteAsync(" <h2>Money</h2>");
 });
 
